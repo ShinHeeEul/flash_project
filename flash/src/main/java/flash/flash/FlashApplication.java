720d066b877101fbb23b533faed9051e0c8066f6
ShinHeeEul <sheshe7015@gmail.com>
2023-01-19 13:48:07 +0900

    2023-01-19
    프로젝트 설정

--- flash/src/main/java/flash/flash/FlashApplication.java
+++ flash/src/main/java/flash/flash/FlashApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FlashApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FlashApplication.class, args);
+
 	}
 
 
